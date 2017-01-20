a87e037888eed9a1472d593e983f9b5b22181219
DanusiaZabcia <ozesek@wp.pl>
2017-01-20 14:44:41 +0100

    Pracownicy

---
+++ Obiektowe/src/Zadanie1obiektowe/Firma.java
@@ -0,0 +1,43 @@
+package Zadanie1obiektowe;
+import java.util.Scanner;
+
+public class Firma {
+
+	public static void main(String[] args) {
+	
+		int x;
+		Scanner odczyt = new Scanner(System.in);
+		System.out.println("Proszê podaæ ilu pracownikow chcesz wprowadziæ");
+		x = odczyt.nextInt();
+		int y = x;
+		odczyt.nextLine();
+		Pracownik[] pracownik = new Pracownik[x];
+		
+		while (x>0){
+			x--;
+		pracownik[x] = new Pracownik();
+		System.out.println("Podaj imiê");
+		pracownik[x].imie = odczyt.nextLine();
+		odczyt.nextLine();
+		
+		System.out.println("Podaj nazwisko");
+		pracownik[x].nazwisko = odczyt.nextLine();
+		odczyt.nextLine();
+		
+		System.out.println("Podaj wiek");
+		pracownik[x].wiek = odczyt.nextInt();
+		odczyt.nextLine();
+		
+		
+
+		}
+		odczyt.close();
+		
+		while(y>0){
+			y--;
+			System.out.println("Twój pracownik numer " + y + "to" + pracownik[y].imie + pracownik[y].nazwisko + " i ma" + pracownik[y].wiek +" lat");
+		
+		}
+	}
+
+}
---
+++ Obiektowe/src/Zadanie1obiektowe/Pracownik.java
@@ -0,0 +1,15 @@
+
+package Zadanie1obiektowe;
+
+
+
+
+public class Pracownik {
+
+	
+	String imie;
+	
+	String nazwisko;
+	
+	int wiek;
+}
