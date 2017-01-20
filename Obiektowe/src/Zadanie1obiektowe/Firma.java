89141f0c7b0dac2e96ff5255ff093f02725dbf0f
DanusiaZabcia <ozesek@wp.pl>
2017-01-20 14:48:52 +0100

    details of Firma

--- Obiektowe/src/Zadanie1obiektowe/Firma.java
+++ Obiektowe/src/Zadanie1obiektowe/Firma.java
@@ -1,10 +1,11 @@
 package Zadanie1obiektowe;
+
 import java.util.Scanner;
 
 public class Firma {
 
 	public static void main(String[] args) {
-	
+
 		int x;
 		Scanner odczyt = new Scanner(System.in);
 		System.out.println("Proszê podaæ ilu pracownikow chcesz wprowadziæ");
@@ -12,31 +13,30 @@ public class Firma {
 		int y = x;
 		odczyt.nextLine();
 		Pracownik[] pracownik = new Pracownik[x];
-		
-		while (x>0){
+
+		while (x > 0) {
 			x--;
-		pracownik[x] = new Pracownik();
-		System.out.println("Podaj imiê");
-		pracownik[x].imie = odczyt.nextLine();
-		odczyt.nextLine();
-		
-		System.out.println("Podaj nazwisko");
-		pracownik[x].nazwisko = odczyt.nextLine();
-		odczyt.nextLine();
-		
-		System.out.println("Podaj wiek");
-		pracownik[x].wiek = odczyt.nextInt();
-		odczyt.nextLine();
-		
-		
+			pracownik[x] = new Pracownik();
+			System.out.println("Podaj imiê");
+			pracownik[x].imie = odczyt.nextLine();
+			odczyt.nextLine();
+
+			System.out.println("Podaj nazwisko");
+			pracownik[x].nazwisko = odczyt.nextLine();
+			odczyt.nextLine();
+
+			System.out.println("Podaj wiek");
+			pracownik[x].wiek = odczyt.nextInt();
+			odczyt.nextLine();
 
 		}
 		odczyt.close();
-		
-		while(y>0){
+
+		while (y > 0) {
 			y--;
-			System.out.println("Twój pracownik numer " + y + " to " + pracownik[y].imie +" , " + pracownik[y].nazwisko + " a jego wiek to " + pracownik[y].wiek );
-		
+			System.out.println("Twój pracownik numer " + y + " to " + pracownik[y].imie + " , " + pracownik[y].nazwisko
+					+ " a jego wiek to " + pracownik[y].wiek);
+
 		}
 	}
 
