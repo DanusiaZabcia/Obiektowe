47bf5f2f674dc2713190b5baef29e34e5c19371e
DanusiaZabcia <ozesek@wp.pl>
2017-01-27 16:07:59 +0100

    strange mistake

---
+++ Obiektowe/src/Zadanie4/Przeciaz.java
@@ -0,0 +1,26 @@
+package Zadanie4;
+
+public class Przeciaz {
+
+	
+	
+	int dajX(int a,int b){
+		
+		return a+b;
+	}
+		double dajX(double c, double d){
+			
+	return c+d;	
+	
+		}
+	long dajX(long e, long f){
+		return e+f;
+	}
+	
+	short dajX(short g, short h){
+		return g+h;
+}
+	}
+		
+		
+
