710cad62a960ae91308d38bd215c6c8ce9b5beb5
DanusiaZabcia <ozesek@wp.pl>
2017-01-31 15:41:03 +0100

    Coordinates new way

---
+++ Obiektowe/src/Zadanie5/Coordinates.java
@@ -0,0 +1,14 @@
+package Zadanie5;
+
+public class Coordinates {
+
+	public static void main(String[] args) {
+		
+		Kon punkt1 = new Kon(10,30);
+		System.out.println("wspó³rzêdne x to "+ punkt1.x+" wspó³rzêdne y to "+ punkt1.y);
+		
+		Kon punkt2 = new Kon(16,20);
+		System.out.println("wspó³rzêdne x to "+ punkt2.x+" wspó³rzêdne y to "+ punkt2.y);
+	}
+
+}
