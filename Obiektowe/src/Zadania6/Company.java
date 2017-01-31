706f3798a5f8d969ccaaa78fef0798d197755f65
DanusiaZabcia <ozesek@wp.pl>
2017-01-31 16:57:34 +0100

    complete employees

--- Obiektowe/src/Zadania6/Company.java
+++ Obiektowe/src/Zadania6/Company.java
@@ -13,6 +13,14 @@ public class Company {
 			System.out.println(first[n].name + first[n].surname + first[n].age);
 			n++;
 		}
+Employee[] second = new Employee[3];
 
+second[0]= new Employee("Sherlock", "Holmes", 28);
+second[1]= new Employee("John", "Watson", 30);
+second[2]= new Employee(second[0]);
+
+for(int i =0; i<3;i++){
+	System.out.println(second[i].name + second[i].surname + second[i].age);
+}
 	}
 }
