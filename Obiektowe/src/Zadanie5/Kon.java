47a33be51b2ad2a81d8a748347dbe99552067d93
DanusiaZabcia <ozesek@wp.pl>
2017-01-31 15:33:47 +0100

    constructor

---
+++ Obiektowe/src/Zadanie5/Kon.java
@@ -0,0 +1,19 @@
+package Zadanie5;
+
+public class Kon {
+
+	int x;
+	int y;
+
+	public Kon() {
+	}
+
+	public Kon(int a, int b) {
+		x = a;
+		y = b;
+	}
+ public Kon (Kon pkt){
+	x=pkt.x;
+	y=pkt.y;
+ }
+}
