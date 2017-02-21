c1555a3a52fd2387f8d64675636e9a9958a2bb98
Danusia Żabińska <danuta.zabinska@ebicom.pl>
2017-02-21 15:39:36 +0100

    Bubble done

--- Obiektowe/src/ODKamila/ListSort.java
+++ Obiektowe/src/ODKamila/ListSort.java
@@ -19,17 +19,17 @@ public class ListSort {
 		}
 
 		if (list.size() > 0) {
+			for (int x = 0; x < list.size() - 1; x++) {
+				for (int i = 0; i < list.size() - 1; i++) {
 
-			for (int i = 0; i < list.size()-1; i++) {
-
-				if (list.get(i).compareTo(list.get(i + 1)) > 0) {
-					Integer temp;
-					temp = list.get(i);
-					list.set(i, list.get(i + 1));
-					list.set(i + 1, temp);
+					if (list.get(i).compareTo(list.get(i + 1)) > 0) {
+						Integer temp;
+						temp = list.get(i);
+						list.set(i, list.get(i + 1));
+						list.set(i + 1, temp);
+					}
 				}
 			}
-
 		}
 
 		System.out.println("" + list);
