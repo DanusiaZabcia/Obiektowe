a823213d6bbfae4231fd5ec3784aa1b9f36658ba
Danusia Żabińska <danuta.zabinska@ebicom.pl>
2017-02-21 15:31:16 +0100

    Bubble sort

---
+++ Obiektowe/src/ODKamila/ListSort.java
@@ -0,0 +1,39 @@
+package ODKamila;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+public class ListSort {
+
+	public static void main(String[] args) {
+
+		ArrayList<Integer> list = new ArrayList<Integer>();
+
+		Random mix = new Random();
+
+		for (int i = 0; i < 30; i++) {
+			Integer a = mix.nextInt(898);
+			list.add(a);
+
+		}
+
+		if (list.size() > 0) {
+
+			for (int i = 0; i < list.size()-1; i++) {
+
+				if (list.get(i).compareTo(list.get(i + 1)) > 0) {
+					Integer temp;
+					temp = list.get(i);
+					list.set(i, list.get(i + 1));
+					list.set(i + 1, temp);
+				}
+			}
+
+		}
+
+		System.out.println("" + list);
+
+	}
+
+}
---
+++ Obiektowe/src/ODKamila/MyObject.java
@@ -0,0 +1,8 @@
+package ODKamila;
+
+public class MyObject {
+
+	
+	
+	
+}
